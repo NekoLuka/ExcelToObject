815675bcef8dfd383516b6eb449980f8a8f42f2b
Luka de Jong <lukadejong2002@gmail.com>
2024-07-05 17:57:21 +0200

    Endpoint created

---
+++ vormer/Controllers/excelController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vormer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class excelController : ControllerBase
+    {
+        [HttpPost(Name = "upload")]
+        public string Upload()
+        {
+            return "Hello world";
+        }
+    }
+}
